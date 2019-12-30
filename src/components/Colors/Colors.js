cf55b4e9fcd19694268d981cbcd940697ce081ea
Josh Ganote <j.ganote0825@gmail.com>
2019-12-29 22:33:25 -0600

    setup both components with default info and imported a header component into each of them

---
+++ src/components/Colors/Colors.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import HeaderColors from '../HeaderColors/HeaderColors';
+
+class Colors extends Component {
+
+    render() {
+        return(
+            <div>
+                <HeaderColors />
+            </div>
+        )
+    }
+}
+
+export default Colors;
---
+++ src/components/Swatches/Swatches.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from '../Header/Header';
+
+class Swatches extends Component {
+
+    render() {
+        return(
+            <div>
+                 <Header />
+            </div>
+        )
+    }
+}
+
+export default Swatches;
