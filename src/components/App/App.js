6f33cacbf43506219ff4174f6e463f9335a0efe6
Josh Ganote <j.ganote0825@gmail.com>
2019-12-29 22:29:07 -0600

    created two Link and two Route paths for swatches and color components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 // COMPONENTS
-import Header from '../Header/Header';
+import Swatches from '../Swatches/Swatches';
+import Colors from '../Colors/Colors';
+
+
+// ROUTES
+
 
 class App extends Component {
     render() {
         return (
             <div>
-                <Header />
+                <Router>
+                <Route exact path="/" component={Swatches} />
+                        <Route exact path="/colors/" component={Colors} />
+                    <div>
+                        <ul>
+                            <li>
+                                <Link to="/">Swatches</Link>
+                            </li>
+                            <li>
+                                <Link to="/colors/">Colors</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="container">
+                        PAGE CONTENT
+                    </div>
 
-                <div className="container">
-                    PAGE CONTENT
-                </div>
+                </Router>
             </div>
         );
     }
