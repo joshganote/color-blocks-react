c21eb3ccd117e8bea18ce16a21b97c1cf9ca2ff9
Josh Ganote <j.ganote0825@gmail.com>
2019-12-29 22:34:14 -0600

    wanted to keep the same look for the header on each page so copied the original header component and made one for the colors page

---
+++ src/components/HeaderColors/HeaderColors.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './HeaderColors.css';
+
+class HeaderColors extends Component {
+    render() {
+        return (
+            <header className="appBar">
+                <h1>Color Settings</h1>
+            </header>
+        );
+    }
+}
+
+export default HeaderColors;
